7e3a239282ce3421aae5b0833be701dfc04ec2f7
tahacabello <tahacabello@github.com>
2026-06-01 16:08:56 +0200

    Fix next config format for GitHub actions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/jaguar-occasions',
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
